02165d1796e0c1584f8b53fcfa28fff7bb0968a1
19046498 <19046498@LAPTOP-K9M0TGBJ>
2020-08-18 10:58:52 +0800

    Added Helper.java and started on C206_CaseStudy

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,44 @@
-
-public class C206_CaseStudy {
-
-	public static void main(String[] args) {
+public class C206_CaseStudy
+{
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
-
+		
+		int option = 0;
+		
+		while (option != 3)
+		{
+			C206_CaseStudy.menu();
+			option = Helper.readInt("Enter an option > ");
+			
+			if (option == 1)
+			{
+				
+			}
+		}
+	}
+	
+	
+	public static void menu()
+	{
+		C206_CaseStudy.setHeader("RESOURCE CENTRE APP");
+		System.out.println("1. Login as Customer");
+		System.out.println("2. Login as Admin/Designers");
+		System.out.println("3. Exit");
+		Helper.line(80, "-");
 	}
 
+
+	private static void setHeader(String header)
+	{
+		// TODO Auto-generated method stub
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
+	}
+	
+	public static void showMenuCustomer()
+	{
+		
+	}
 }
