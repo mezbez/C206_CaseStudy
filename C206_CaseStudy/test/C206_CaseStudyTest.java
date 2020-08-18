dfd6a7b378fb1e6c6787d0f9624f3ea504e8bb50
19045168 <19045168@NSNP-TH0NK>
2020-08-18 12:01:51 +0800

    Added variables for CaseStudyTest.

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,13 +1,38 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
+	private Request request01;
+	private Request request02;
+	private Request request03;
+	private Customer customer01;
+	private Customer customer02;
+	private Customer customer03;
+	private userAccount userAccount01;
+	private userAccount userAccount02;
+	private userAccount userAccount03;
+	private Appointment appointment01;
+	private Appointment appointment02;
+	private Appointment appointment03;
+	
+	private ArrayList<Request> requestList;
+	private ArrayList<Customer> customerList;
+	private ArrayList<userAccount> userAccountList;
+	private ArrayList<Appointment> appointmentList;
 
+	public C206_CaseStudyTest() {
+		super();
+	}
+	
 	@Before
 	public void setUp() throws Exception {
+		
+		
 	}
 
 	@After
