eb9abf0f7aef7d841d39c4f3856317ee12ca1336
19029863 <19029863@LAPTOP-9KN5BD65>
2020-08-18 11:30:33 +0800

    case study

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,71 +1,50 @@
-public class C206_CaseStudy
-{
-	public static void main(String[] args)
-	{
+public class C206_CaseStudy {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		int option = 0;
-		
-		while (option != 3)
-		{
+
+		while (option != 3) {
 			C206_CaseStudy.menu();
 			option = Helper.readInt("Enter an option > ");
-			
-			if (option == 1)
-			{
+
+			if (option == 1) {
 				C206_CaseStudy.showMenuCustomer();
 				int customerMenu = Helper.readInt("Enter an option which suits your needs: ");
-				
-				if (customerMenu == 1)
-				{
+
+				if (customerMenu == 1) {
 					System.out.println("customerMenu1");
-				}
-				else if (customerMenu == 2)
-				{
+				} else if (customerMenu == 2) {
 					System.out.println("customerMenu2");
-				}
-				else
-				{
+				} else {
 					System.out.println("Invalid option");
 				}
-			}
-			else if(option == 2)
-			{
+			} else if (option == 2) {
 				C206_CaseStudy.showMenuAD();
 				int adMenu = Helper.readInt("Enter an option which suit your needs: ");
-				
-				if (adMenu == 1)
-				{
+
+				if (adMenu == 1) {
 					System.out.println("adMenu1");
-				}
-				else if (adMenu == 2)
-				{
+				} else if (adMenu == 2) {
 					System.out.println("adMenu2");
-				}
-				else if (adMenu == 3)
-				{
+				} else if (adMenu == 3) {
 					System.out.println("adMenu3");
-				}
-				else if (adMenu == 4)
-				{
+				} else if (adMenu == 4) {
 					System.out.println("adMenu4");
-				}
-				else if (adMenu == 5)
-				{
+				} else if (adMenu == 5) {
 					System.out.println("adMenu5");
-				}
-				else
-				{
+				} else {
 					System.out.println("Invalid options");
 				}
-					
+
+			} else {
+				System.out.println("Thank you for using RENOVATION ACE!");
+
 			}
 		}
 	}
-	
-	
-	public static void menu()
-	{
+
+	public static void menu() {
 		C206_CaseStudy.setHeader("RENOVATION ACE APP");
 		System.out.println("1. Login as Customer");
 		System.out.println("2. Login as Admin/Designers");
@@ -73,24 +52,21 @@ public class C206_CaseStudy
 		Helper.line(80, "-");
 	}
 
-
-	private static void setHeader(String header)
-	{
+	private static void setHeader(String header) {
 		// TODO Auto-generated method stub
 		Helper.line(80, "-");
 		System.out.println(header);
 		Helper.line(80, "-");
 	}
-	
-	public static void showMenuCustomer()
-	{
+
+	public static void showMenuCustomer() {
 		C206_CaseStudy.setHeader("Customer");
 		System.out.println("1. Request for Quotation");
 		System.out.println("2. Manage Appointment");
+		System.out.println("3. End");
 	}
-	
-	public static void showMenuAD()
-	{
+
+	public static void showMenuAD() {
 		C206_CaseStudy.setHeader("Customer");
 		System.out.println("1. Manage Customer");
 		System.out.println("2. Manage Package");
